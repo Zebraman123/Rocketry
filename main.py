a0ef81e76deb54ee9b52eb6800ac076e83458e61
Zebraman123 <lilluk3tx@gmail.com>
2024-01-31 22:29:03 -0600

    everything but com

--- main.py
+++ main.py
@@ -8,17 +8,16 @@ env = Environment(
 )
 
 #Year, Month, Day, Hour
-tomorrow = datetime.date.today() + datetime.timedelta(days=1)
-
 env.set_date(
-    (tomorrow.year, tomorrow.month, tomorrow.day, 12)
+    (2023, 11, 18, 12)
 )
 
 #atmospheric data
 env.set_atmospheric_model(type="Forecast", file="GFS")
 
-
+#Expected Apogee of rocket
 env.max_expected_height = 1000
+
 #shows enviromental information based on location and date
 env.info()
 
@@ -30,6 +29,7 @@ env.info()
 #in kg
 #mass = 1.119806
 
+#Rocket Body
 OneL = Rocket(
     mass = 1.119806,
     radius = 0.04,
@@ -40,31 +40,29 @@ OneL = Rocket(
     power_on_drag ="/mnt/c/Users/zebra/Documents/Rocketry/CDLONE.txt"
 )
 
+#nose
 nose_cone = OneL.add_nose(length=0.3302, kind="elliptical", position=0)
 
+#Fin
 fin_set = OneL.add_trapezoidal_fins(n=4,root_chord=0.135,tip_chord=0.089, span=0.066,position=-1,cant_angle=0.86)
-
-
-
- #   dry_inertia=(148/10000000, 148/10000000,0.000679),
 
 #Motor 
 H97 = SolidMotor(
     thrust_source="/mnt/c/Users/zebra/Documents/Rocketry/LONETHRUSTCURVE.csv",
-    dry_mass=0.141,
-    dry_inertia=(0, 0,0),
+    dry_mass=0.145, #0.282-0.137 from apogeerockets
+    dry_inertia=(0, 0,0), #good
     nozzle_radius=29 / 1000,
-    grain_number=4,
-    grain_density=2085.96,
-    grain_outer_radius=29 / 1000,
-    grain_initial_inner_radius=15 / 1000,
-    grain_initial_height=120 / 1000,
-    grain_separation=0 / 1000,
-    grains_center_of_mass_position=0.397,
+    grain_number=4, #good
+    grain_density=2085.96, #good
+    grain_outer_radius=11.85 / 1000, #OD is 0.933in based on number 8 from the motor assembly
+    grain_initial_inner_radius=0.50395 / 1000, #based off meeting notes, no idea where this came from
+    grain_initial_height=44.45 / 1000, #Part number 8 length
+    grain_separation=0 / 1000, #Picture shows no sep
+    grains_center_of_mass_position=0.397, 
     center_of_dry_mass_position=0.317,
     nozzle_position=0,
-    burn_time=3.9,
-    throat_radius=31 / 1000,
+    burn_time=2.2,#from apogeerockets
+    throat_radius=7.93 / 1000, #good
     coordinate_system_orientation="nozzle_to_combustion_chamber",
 )
 
